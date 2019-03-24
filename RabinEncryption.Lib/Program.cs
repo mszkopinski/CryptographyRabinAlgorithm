1cb93dbbb49c0c08c15a64805eb354565c10903c
Michal <michal_szkopinski@op.pl>
2019-03-24 12:21:07 +0100

    Rabin encryption and decryption should now work properly. Extracted ExtendedEuclidanAlgorithm for eadability purposes

--- RabinEncryption.Lib/Program.cs
+++ RabinEncryption.Lib/Program.cs
@@ -1,9 +1,30 @@
-﻿namespace RabinEncryption.Lib
+﻿using System;
+using System.Linq;
+
+namespace RabinEncryption.Lib
 {
     class Program
     {
+        const int P = 167, Q = 151;
+        const int CompositeNumber = P * Q;
+
         static void Main(string[] args)
         {
+            string msg = "SAMPLE MESSAGE";
+            var encryptedMessage = msg
+                .Select(c => Rabin.Rabin.Encrypt(c, CompositeNumber))
+                .ToList();
+
+            for (int i = 0; i < encryptedMessage.Count; ++i)
+            {
+                Console.WriteLine(encryptedMessage[i]);
+            }
+
+            var decryptedMessage = encryptedMessage
+                .Select(e => Rabin.Rabin.Decrypt(e, P, Q))
+                .ToList();
+
+            Console.WriteLine(Rabin.Rabin.DecodeMessage(decryptedMessage));
         }
     }
 }
---
+++ RabinEncryption.Lib/Rabin/ExtendedEuclideanAlgorithm.cs
@@ -0,0 +1,33 @@
+﻿namespace RabinEncryption.Lib.Rabin
+{
+    public static class ExtendedEuclideanAlgorithm
+    {
+        public static int[] Calculate(int a, int b)
+        {
+            if (b > a)
+            {
+                var temp = a;
+                a = b;
+                b = temp;
+            }
+            int x = 0, lastX = 1, y = 1, lastY = 0;
+            while (b != 0)
+            {
+                var q = a / b;
+
+                var temp1 = a % b;
+                a = b;
+                b = temp1;
+
+                var temp2 = x;
+                x = lastX - q * x;
+                lastX = temp2;
+
+                var temp3 = y;
+                y = lastY - q * y;
+                lastY = temp3;
+            }
+            return new[] { lastX, lastY, 1 };
+        }
+    }
+}
